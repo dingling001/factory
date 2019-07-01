9a8f3399438b8b64eece4ed6dfd9dbddd952b625
dl <215594238@qq.com>
2019-07-02 07:30:19 +0800

    7.2

--- assets/js/public.js
+++ assets/js/public.js
@@ -2,19 +2,12 @@
 var baseurl = 'http://j.jianghairui.com/api/'; //加工宝
 var mobileReg = /^1\d{10}$/gi; //手机正则
 var id_card = /^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;//身份证正则
-// var goodUrl = 'http://localhost:8080/A6079080424317/html/';//公众号域名
-var puplic_url = 'http://kindapp.w.bronet.cn/A6079080424317/web_adapter/adapter.html';
+// var puplic_url = 'http://kindapp.w.bronet.cn/A6079080424317/web_adapter/adapter.html';
 // var goodUrl = 'http://kindapp.w.bronet.cn/A6079080424317/'; //测试域名
-var goodUrl = 'http://wx.cijievip.com/A6079080424317/';//正式域名
+// var goodUrl = 'http://wx.cijievip.com/A6079080424317/';//正式域名
 var imgUrl = 'http://j.jianghairui.com/';
-var ERROR = '';
+// var ERROR = '';
 
-function openWin(winName, url) {
-    api.openWin({
-        name: winName,
-        url: url
-    });
-}
 
 function ApiAjax(url, subDatas, callbackfun, file = {}, type = false) {
     // console.log(url);
@@ -125,22 +118,6 @@ function getHeight(id) {
 }
 
 
-function openFm(fmName, url) {
-    var posY = $api.offset($api.dom("header")).h;
-    var fmUrl = url ? url : fmName + "_fm.html";
-    api.openFrame({
-        name: fmName,
-        url: fmUrl,
-        rect: {
-            x: 0,
-            y: posY
-        },
-        bounces: true,
-        hScrollBarEnabled: false,
-        vScrollBarEnabled: false
-    });
-}
-
 function toastMsg(msg) {
     api.toast({
         msg: msg,
@@ -199,33 +176,6 @@ function getToken() {
         return null;
     }
 }
-
-
-function isLogin(_this, callback) {
-    Vue.http.options.emulateJSON = true;
-    var logpost = {
-        cmd: 'u_checkTokenIsRight',
-        token: getToken()
-    };
-    console.log(baseUrl)
-    _this.$http.post(baseUrl + 'Api/Init/', logpost).then(function (res) {
-        if (res.data.code == 1) {
-            callback(true);
-        } else {
-            callback(false);
-        }
-    }, function () {
-        toastMsg('网络小短腿跑不动了...');
-        api.refreshHeaderLoadDone();
-        // api.addEventListener({
-        //   name:'loadfailed'
-        // });
-        losenet(function () {
-            location.reload();
-        });
-    });
-}
-
 function loadEnd() {
     var ele = document.getElementById('loadStart');
     var app = document.getElementById('app');
@@ -239,37 +189,6 @@ function loadEnd() {
     }
 }
 
-function getcity() {
-    if ($api.getStorage('city')) {
-        return $api.getStorage('city')
-    } else {
-        return null;
-    }
-}
-
-//content转化为html
-function charToHtml(str) {
-    if (str) {
-        str = str.replace(/&amp;/g, '&');
-        str = str.replace(/&lt;/g, '<');
-        str = str.replace(/&gt;/g, '>');
-        str = str.replace(/&quot;/g, "'");
-        str = str.replace(/&#039;/g, ' ');
-    }
-    return str;
-}
-
-//我的-头像显示
-refresh = function (callback) {
-    api.setRefreshHeaderInfo({
-        bgColor: '#eee',
-        textColor: '#000',
-        textDown: '下拉刷新...',
-        textUp: '松开刷新...'
-    }, function () {
-        callback();
-    });
-};
 
 function getFileType(filePath) {
     var startIndex = filePath.lastIndexOf(".");
@@ -289,46 +208,6 @@ function getImageSizeInBytes(imgURL) {
     return parseInt(RegExp.$1) || 0;
 }
 
-refreshDone = function (mess) {
-    if (mess) {
-        toastMsg(mess);
-    }
-    setTimeout(function () {
-        api.refreshHeaderLoadDone();
-    }, 500)
-};
-
-// 图片分享
-function formatShareImg(imgUrl) {
-    if (imgUrl) {
-        imgUrl = imgUrl.split(',')[0];
-        if (imgUrl.indexOf('http') !== 0) {
-            imgUrl = baseUrl + imgUrl;
-        }
-    } else {
-        imgUrl = baseUrl + '/Public/applogo.png';
-    }
-
-    return imgUrl;
-}
-
-
-// function data_string(str, value) {
-//     if (value == "yyyy-MM-dd hh:mm:ss") {
-//         var d = eval('new ' + str.substr(1, str.length - 2));
-//         var ar_date = [d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()];
-//         for (var i = 0; i < ar_date.length; i++) ar_date[i] = dFormat(ar_date[i]);
-//         return ar_date.slice(0, 3).join('-') + ' ' + ar_date.slice(3).join(':');
-//         function dFormat(i) { return i < 10 ? "0" + i.toString() : i; }
-//     }
-//     else if (value == "yyyy-MM-dd") {
-//         var d = eval('new ' + str.substr(1, str.length - 2));
-//         var ar_date = [d.getFullYear(), d.getMonth() + 1, d.getDate()];
-//         for (var i = 0; i < ar_date.length; i++) ar_date[i] = dFormat(ar_date[i]);
-//         return ar_date.join('-');
-//         function dFormat(i) { return i < 10 ? "0" + i.toString() : i; }
-//     }
-// }
 
 function add0(num) {
     if (num) {
@@ -430,34 +309,34 @@ function toWan(str) {
         return str
     }
 }
-
-function getRequest(type = 'post', url, params) {
-    var promise = new Promise(function (resolve, reject) {
-        axios({
-            url: baseurl + url,
-            method: type,
-            data: params,
-            headers: {
-                // 'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).then(function (res) {
-            if (res.data.code == 502) {
-                api.openWin({
-                    name: 'login',
-                    url: 'widget://html/login/login.html',
-                });
-            } else {
-                resolve(res);
-                // loadEnd();
-            }
-            // resolve(res)
-        }).catch(function (err) {
-            toastMsg('网络错误');
-            reject(err)
-        })
-    });
-    return promise
-}
+//
+// function getRequest(type = 'post', url, params) {
+//     var promise = new Promise(function (resolve, reject) {
+//         axios({
+//             url: baseurl + url,
+//             method: type,
+//             data: params,
+//             headers: {
+//                 // 'Content-Type': 'application/x-www-form-urlencoded'
+//             }
+//         }).then(function (res) {
+//             if (res.data.code == 502) {
+//                 api.openWin({
+//                     name: 'login',
+//                     url: 'widget://html/login/login.html',
+//                 });
+//             } else {
+//                 resolve(res);
+//                 // loadEnd();
+//             }
+//             // resolve(res)
+//         }).catch(function (err) {
+//             toastMsg('网络错误');
+//             reject(err)
+//         })
+//     });
+//     return promise
+// }
 
 //设备判断
 function getDevice() {
@@ -503,40 +382,4 @@ function getDevice() {
     return login_device
 }
 
-// 动态环
-function giftCircleProgressFn(per) {
-    var circleData = {};
-    var percent = parseInt(per);
-    //领取进度环形颜色className
-    var halfClassName = percent < 50 ? "little" : "more";
-    //左半环遮罩层显示样式状态
-    var leftCircleDisplay = percent < 50 ? "block" : "none";
-    var leftRotate = 0;
-    var rightRotate = 0;
-    //以50%为界限;<50%:右半圆占比为0,左半圆需要使用遮罩进行遮挡,展示剩余部分
-    //           >50%:左半圆占比100%,右半圆直接使用百分比计算所占部分即可
-    //注意：在半圆中计算百分比时,要将百分比乘以2。
-    if (percent < 50) {
-        leftRotate = -15 - 180 + 150 * (percent * 2) / 100;
-        rightRotate = -135;
-    } else {
-        leftRotate = -15;
-        rightRotate = -135 + (150 * ((percent - 50) * 2) / 100);   //比例在半环计算需要*2倍
-    }
-    circleData = {
-        leftRotate: leftRotate,  //左半环进度
-        rightRotate: rightRotate, //右半环进度
-        halfClassName: halfClassName, //50% 进度环 变色
-        leftCircleDisplay: leftCircleDisplay, //左半环遮罩
-        percent: per  //进度百分比
-    }
-    return circleData
-}
-
-function escape2Html(str) {
-    var arrEntities = {'lt': '<', 'gt': '>', 'nbsp': ' ', 'amp': '&', 'quot': '"'};
-    return str.replace(/&(lt|gt|nbsp|amp|quot);/ig, function (all, t) {
-        return arrEntities[t];
-    });
-}
 
